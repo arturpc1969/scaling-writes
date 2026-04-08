04762a814454f500a852bee5d3ad5bcaa49c0d69
Artur Coutinho <arturpc@yahoo.com.br>
2026-04-07 22:08:36 -0300

    SW-3 - Add project base files

---
+++ src/main/java/com/arturpc/scaling_writes/ScalingWritesApplication.java
@@ -0,0 +1,13 @@
+package com.arturpc.scaling_writes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalingWritesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalingWritesApplication.class, args);
+	}
+
+}
